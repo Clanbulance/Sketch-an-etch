9c03603c5d25393b07f3dd1bb8a5549efb9d2751
Clanbulance <107610779+Clanbulance@users.noreply.github.com>
2023-07-11 12:24:28 +0200

    script.js now works with hover

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-let pixels = "16";
+
+let rows = "16";
+let pixels = rows * rows;
+
 let squares = "0"
-const gridWidth = document.querySelector(".drawContainer").clientWidth;
-const gridHeight = document.querySelector(".drawContainer").clientHeight;
-console.log(gridWidth,gridHeight);
 
 const drawContainer = document.querySelector(".drawContainer");
 const pixelAmount = document.getElementById("pixelAmount");
+pixelAmount.innerText = rows;
 
 
-pixelAmount.innerText = pixels;
-
-
-
-
+let pixelSize = 500/(Math.sqrt(pixels))
 
 
 for (x = 0; x < pixels; x++){
     const pixel = document.createElement("div");
     pixel.classList.add("pixel");
+    pixel.style.width = pixelSize + "px"
+    pixel.style.height = pixelSize + "px"
     drawContainer.appendChild(pixel);
-    
-    squares++;
-    console.log(`${squares} squares created`)
-    
+ 
+} 
+console.log(`${pixels} squares created`)
+
+
+const cells = document.querySelectorAll("div.pixel");
+
+cells.forEach(cell => cell.addEventListener('mouseenter', colorCell));
+
+function colorCell(e){
+console.log(e)
+e.target.style.backgroundColor = "black"
 }
+
+
+//old code
+//const gridWidth = document.querySelector(".drawContainer").clientWidth;
+//const gridHeight = document.querySelector(".drawContainer").clientHeight;
+//console.log(gridWidth,gridHeight);
