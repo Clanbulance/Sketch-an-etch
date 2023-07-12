22d458f1508c13a16c9b5e43c969e74b50128099
Clanbulance <107610779+Clanbulance@users.noreply.github.com>
2023-07-12 12:03:00 +0200

    Got slide bar working, changed it so some listeners stay alive

--- script.js
+++ script.js
@@ -1,36 +1,68 @@
 
-let rows = "16";
+//Trying to grab the value of the slider
+const sliderValue = document.querySelector("#pixelAmount");
+const input = document.querySelector("#slider");
+sliderValue.textContent=input.value;
+input.addEventListener("input", (e) => {
+  sliderValue.textContent = e.target.value;
+  console.log(sliderValue.textContent);
+    rows = sliderValue.textContent;
+    pixels = rows * rows;
+    pixelSize = 500/(Math.sqrt(pixels))
+  GenerateCells(pixels)
+});
+
+
+
+let rows = sliderValue.textContent;
 let pixels = rows * rows;
 
 let squares = "0"
 
 const drawContainer = document.querySelector(".drawContainer");
-const pixelAmount = document.getElementById("pixelAmount");
-pixelAmount.innerText = rows;
-
 
 let pixelSize = 500/(Math.sqrt(pixels))
 
-
-for (x = 0; x < pixels; x++){
+//generates the divs at the start
+GenerateCells(pixels)
+function GenerateCells(pixelvalue){
+drawContainer.innerHTML= "";
+for (x = 0; x < pixelvalue; x++){
     const pixel = document.createElement("div");
     pixel.classList.add("pixel");
     pixel.style.width = pixelSize + "px"
     pixel.style.height = pixelSize + "px"
     drawContainer.appendChild(pixel);
- 
-} 
+    const cells = document.querySelectorAll("div.pixel");                   //add listener to cells un function so it does not get removed
+    cells.forEach(cell => cell.addEventListener('mouseenter', colorCell));
+
+}} 
+
+
+
 console.log(`${pixels} squares created`)
 
-
-const cells = document.querySelectorAll("div.pixel");
-
-cells.forEach(cell => cell.addEventListener('mouseenter', colorCell));
+// This part makes a nodelist for all the cells and // this is now in the GenerateCells function, so it stays alive
+// This is now just the color function
 
 function colorCell(e){
 console.log(e)
 e.target.style.backgroundColor = "black"
 }
+
+//Here we make a button to clear all the cells
+const clearButton = document.getElementById("clearButton");
+clearButton.addEventListener('click',clearCells);
+
+function clearCells(){
+    const cells = document.querySelectorAll("div.pixel");  // added this so it stays alive                 
+    cells.forEach(cell => cell.style.backgroundColor = "white")
+};
+
+
+
+
+
 
 
 //old code
